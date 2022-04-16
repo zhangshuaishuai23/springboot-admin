0c0bc1a1b069a35766d999e96cdb4dd71980a8e2
zhangshuaishuai <1054936014@qq.com>
2022-04-16 22:14:34 +0800

    测试提交222

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # springboot-admin
 In this repository,I'll start a project to show how to build a SpringBoot frame
 
-一名普通java开发者，利用闲暇时间集成springboot和一些主流框架，自我记录一下
+一名普通java开发者，利用闲暇时间集成springboot和一些主流框架，自我记录一下1111
--- src/main/java/com/example/shuai/mapper/UserMapper.java
+++ src/main/java/com/example/shuai/mapper/UserMapper.java
@@ -1,9 +1,13 @@
 package com.example.shuai.mapper;
 
-import com.example.shuai.system.user.entity.User;
+import com.example.shuai.system.entity.User;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserMapper {
     User findById(Integer id);
+
+    @Select("SELECT * FROM sys_user WHERE userName = #{name}")
+    User selectByName(String name);
 }
